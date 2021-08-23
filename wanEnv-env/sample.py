c051959d9bab03c57c55173e84e713eee909f6ed
sollarp <szpitor@gmail.com>
2021-08-23 23:33:34 +0100

    improve

--- wanEnv-env/sample.py
+++ wanEnv-env/sample.py
@@ -1,4 +1,21 @@
+
 with open("wanEnv-env/file_example_WAV_1MG.wav","rb") as f:
     data = f.read()
-    
-print(data[3])
+    n = 0
+    list = []
+    for i in range(len(data)):
+        dataHex = hex(data[n])[2:]
+        list.append(dataHex)
+        n += 1            
+    #print(list[0:70])
+
+    listToStr = ''.join(map(str, list))
+    print(listToStr[0:70])
+
+    g = bytes.fromhex(dataHex).decode('utf-8')
+    print(g)
+
+
+
+
+
