9cd22d7474ab71129dbbba3c6c2202eb53eedcfe
sollarp <szpitor@gmail.com>
2021-08-24 21:39:32 +0100

    decoded list to str

--- wanEnv-env/sample.py
+++ wanEnv-env/sample.py
@@ -4,9 +4,9 @@ import binascii
 with open("wanEnv-env/file_example_WAV_1MG.wav","rb") as f:
     data = f.read()
     dataHex = binascii.hexlify(data)
-    listByte = dataHex[0:64]
+    listByte = dataHex[0:300]
     listStr = listByte.decode()
-    g = bytearray.fromhex(listStr).decode()
+    g = bytearray.fromhex(listStr).decode("ISO-8859-1")
     print(g)
 
 
