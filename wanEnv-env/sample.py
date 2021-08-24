c34659d2d00e7a6e1f65b55ccbf92b5c3892c0a4
sollarp <szpitor@gmail.com>
2021-08-24 14:24:18 +0100

    hex to binary to string

--- wanEnv-env/sample.py
+++ wanEnv-env/sample.py
@@ -1,18 +1,12 @@
+import binascii
+
 
 with open("wanEnv-env/file_example_WAV_1MG.wav","rb") as f:
     data = f.read()
-    n = 0
-    list = []
-    for i in range(len(data)):
-        dataHex = hex(data[n])[2:]
-        list.append(dataHex)
-        n += 1            
-    #print(list[0:70])
-
-    listToStr = ''.join(map(str, list))
-    print(listToStr[0:70])
-
-    g = bytes.fromhex(dataHex).decode('utf-8')
+    dataHex = binascii.hexlify(data)
+    listByte = dataHex[0:64]
+    listStr = listByte.decode()
+    g = bytearray.fromhex(listStr).decode()
     print(g)
 
 
