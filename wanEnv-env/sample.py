7cee0b80ca6bee9447e71c0944d595df7be04b16
sollarp <szpitor@gmail.com>
2021-08-26 09:00:43 +0100

    follow pep8

--- wanEnv-env/sample.py
+++ wanEnv-env/sample.py
@@ -1,26 +1,41 @@
 import binascii
 
 
-def readfile():
+hexRange = 100
+dataRange = 24
+
+def read_file():
     with open("wanEnv-env/file_example_AVI_1920_2_3MG.avi","rb") as f:
         data = f.read()
-        dataHex = binascii.hexlify(data)
-        dataByte = dataHex[0:100]
-        dataStr = dataByte.decode()
+        dataHex = read_hex(data)
+        dataByte = dataHex[0:hexRange]
         #print(dataStr[19:32])
-        fileExt = bytearray.fromhex(dataStr[10:24]).decode("ISO-8859-1")
+        dataStr = bytes_to_str(dataByte)
+        fileExt = decode_file(dataStr)
         if "WAVE" in fileExt:
             print(fileExt)
         elif "AVI" in fileExt:
             print('This file avi')
         #proba(dataStr)
 
-def proba(dataStr):
-    print(dataStr)
+
+def bytes_to_str(dataByte):
+    dataStr = dataByte.decode()
+    return dataStr
+
+def read_hex(data):
+    dataHex = binascii.hexlify(data)
+    return dataHex
+
+
+
+def decode_file(dataStr):
+    fileExt = bytearray.fromhex(dataStr[10:dataRange]).decode("ISO-8859-1")
+    return fileExt
 
 
 if __name__ == "__main__":
-    readfile()
+    read_file()
     
 
 
